7248429eb92ac3695c53805ac6207eac6f8af1df
AngelSun <spxcool12520@163.com>
2017-03-03 14:30:26 +0800

    Initial Commit

---
+++ ASQRCodeScanDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ASQRCodeScanDemo
+//
+//  Created by 孙攀翔 on 2017/3/3.
+//  Copyright © 2017年 AngelSunpx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ASQRCodeScanDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ASQRCodeScanDemo
+//
+//  Created by 孙攀翔 on 2017/3/3.
+//  Copyright © 2017年 AngelSunpx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
